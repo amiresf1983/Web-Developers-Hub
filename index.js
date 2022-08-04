96ebae94e9ff5ad50b84b17216d3c21a478e7074
Amir <amir.esf1983@gmail.com>
2022-08-04 16:04:58 +1000

    changes

--- index.js
+++ index.js
@@ -13,13 +13,10 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req, pubsub }),
 });
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/your-database-name",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 // mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
 //   console.log("MongoDB Connected successfully!");
 //   return server.listen({ port: 5000 });
