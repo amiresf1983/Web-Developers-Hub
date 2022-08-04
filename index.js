a81bd637a7dff74e8448e2906b143aca25e4da72
Amir <amir.esf1983@gmail.com>
2022-08-04 14:51:38 +1000

    change2

--- index.js
+++ index.js
@@ -13,10 +13,21 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req, pubsub }),
 });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
+  console.log("MongoDB Connected successfully!");
+  return server.listen({ port: 5000 });
 });
+// .then((res) => {
+//   console.log(`Server successfully running at ${res.url}`);
+// });// mongoose
+// .connect(MONGODB, { useNewUrlParser: true })
+// .then(() => {
+//   console.log("MongoDB Connected successfully!");
+//   return server.listen({ port: 5000 });
+// })
+// .then((res) => {
+//   console.log(`Server successfully running at ${res.url}`);
+// });
 
 // mongoose
 // .connect(MONGODB, { useNewUrlParser: true })
@@ -26,4 +37,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", {
 // })
 // .then((res) => {
 //   console.log(`Server successfully running at ${res.url}`);
+// });
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
 // });
